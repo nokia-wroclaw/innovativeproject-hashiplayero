ca1385eab62f72bc70a31f2e7fadf63c8ca78ea0
Pawel <pawir4@wp.pl>
2022-04-11 22:09:57 +0200

    Add websocket in UI

--- ui/src/pages/FindRoom.tsx
+++ ui/src/pages/FindRoom.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const FindRoom = () => (
+const FindRoom = () => {
+  const socket = new WebSocket("ws://localhost:3001/ws/");
+
+  const [message, setMessage] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+
+  const handleMessage = () => {
+      socket.send(JSON.stringify({
+          message: input
+      }))
+  };
+
+  const handleSetInput = () => {
+    setInput(input);
+  };
+
+  useEffect(() => {
+    socket.onopen = () => {
+      console.log("Connect");
+    };
+
+    socket.onmessage = (e) => {
+      let mess = [...message];
+      mess.push("Message: " + e.data);
+      setMessage(mess);
+    };
+
+    return () => {
+      socket.close();
+    };
+  });
+
+  return (
     <div>
-        Find a room - Lista pokoji
+      <input type="text"  onChange={handleSetInput} />
+    <button onClick={handleMessage}>SEND</button>
+      <div>
+        {message.map((value) => {
+          return <div> {value} </div>;
+        })}
+      </div>
     </div>
-);
+  );
+};
 
 export default FindRoom;
