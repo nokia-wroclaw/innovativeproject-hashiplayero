5e52757fec7aa6328528dc2f932df816bd2cf653
rodziewiczpiotr <piotr.rodziewicz06@gmail.com>
2022-04-23 22:17:35 +0200

    Implement basic functionality for rooms and clients
    Co-authored-by: Mateusz Marciniec <mateuszmar2@gmail.com>

---
+++ internal/server/room.go
@@ -0,0 +1,43 @@
+package server
+
+import "github.com/gorilla/websocket"
+
+type Room struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func newRoom() *Room {
+	return &Room{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (r *Room) run() {
+	for {
+		select {
+		case client := <-r.register:
+			r.clients[client] = true
+		case client := <-r.unregister:
+			if _, ok := r.clients[client]; ok {
+				delete(r.clients, client)
+				close(client.send)
+			}
+		case message := <-r.broadcast:
+			j = addRoom(message, websocket.TextMessage)
+			for client := range r.clients {
+				select {
+				case client.send <- j:
+				default:
+					close(client.send)
+					delete(r.clients, client)
+				}
+			}
+		}
+	}
+}
