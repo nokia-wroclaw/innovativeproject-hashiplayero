fd36d6a61cb8ca164659538b2a03f8597af81731
rodziewiczpiotr <piotr.rodziewicz06@gmail.com>
2022-04-26 17:47:17 +0200

    Add creating room and sending updates about rooms
    Added uuid and name fields in client to recognize him,
    these fields are sent when websocket connection is established.
    New rooms are created based on input json.
    Information about all rooms is sent whenever room has changed.
    Changed AllowOrigin because wildcard will work on websocket.
    Increased amount of generated nodes in board.
    
    Co-authored-by: Mateusz Marciniec <mateuszmar2@gmail.com>

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"encoding/json"
-	"fmt"
-	"innovativeproject-hashiplayero/hashi"
 	"log"
 	"net/http"
 
@@ -12,81 +9,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type BoardSize int
-
-var j []byte
-
-// TODO trzeba się zastanowić czy klucz ma być stringiem czy intem
-var roomsData = make(map[string]RoomJson)
-
-const (
-	SmallSize  BoardSize = 7
-	MediumSize BoardSize = 10
-	BigSize    BoardSize = 15
-)
-
-type Difficulty int
-
-const (
-	Easy Difficulty = iota
-	Medium
-	Hard
-)
-
-type BoardSettings struct {
-	Difficulty `json:"difficulty"`
-	BoardSize  `json:"size"`
-}
-
-type BoardData struct {
-	BoardSettings `json:"settings"`
-	Array         []int `json:"array"`
-}
-
-type RoomJson struct {
-	IdPokoju         string   `json:"idPokoju"`
-	NazwaPokoju      string   `json:"nazwaPokoju"`
-	Prywatny         string   `json:"prywatny"`
-	LimitCzasNaPokoj int      `json:"limitCzasNaPokoj"`
-	HasloDostepu     string   `json:"hasloDostepu"`
-	Czlonkowie       []string `json:"czlonkowie"`
-	LimitCzlonkow    int      `json:"limitCzlonkow"`
-	Rozmiar          int      `json:"rozmiar"`
-}
-
-func HandleBoardData(c *gin.Context) {
-	bs := BoardSettings{}
-	err := c.BindJSON(&bs)
-	if err != nil {
-		text := fmt.Sprintf("Failed to read user data from request: %s", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": text})
-	} else {
-		board := hashi.GenerateBoard(int(bs.BoardSize), int(bs.BoardSize), int(bs.BoardSize/2), 0.5)
-		boardArray := make([]int, bs.BoardSize*bs.BoardSize)
-		for i, n := range board.Nodes {
-			boardArray[i] = n.Bridges
-		}
-		bd := BoardData{bs, boardArray}
-		c.JSON(http.StatusOK, bd)
-	}
-}
-
-func addRoom(message []byte, mt int) []byte {
-	var roomJson RoomJson
-	json.Unmarshal([]byte(message), &roomJson)
-	//TODO dodać funkcjonalność automatycznego dodawania indeksów
-	roomsData[roomJson.IdPokoju] = roomJson
-	j, _ = json.MarshalIndent(roomsData, "", "  ")
-	return j
-}
+var roomsMap = make(map[string]*Room)
+var clientsMap = make(map[string]*Client)
 
 func setRouter() *gin.Engine {
 	// Creates default gin router with Logger and Recovery middleware already attached
 	router := gin.Default()
-	defaultRoom := newRoom()
-	go defaultRoom.run()
+	lobby := newRoom(RoomSettings{Name: "lobby"})
+	roomsMap["lobby"] = lobby
+	go lobby.run()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"OPTIONS", "GET", "POST"},
 		AllowHeaders:     []string{"*"},
 		AllowCredentials: true,
@@ -98,14 +31,10 @@ func setRouter() *gin.Engine {
 		log.Fatal(err)
 	}
 
-	// Create API route group
-	api := router.Group("/api")
-	api.POST("/puzzle", HandleBoardData)
-
 	// Create the websocket route group
 	ws := router.Group("/ws")
 	ws.GET("/", func(c *gin.Context) {
-		serveWs(defaultRoom, c.Writer, c.Request)
+		serveWs(lobby, c.Writer, c.Request)
 	})
 
 	router.GET("/static/", gin.WrapH(http.FileServer(appBox.HTTPBox())))
