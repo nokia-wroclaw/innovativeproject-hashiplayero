09d1f76c8bce52a3280bbc12a13d7b8c5f947f0d
Rafal Rodak <rafalrodak00@gmail.com>
2022-04-27 01:55:18 +0200

    Change FAQ component

--- ui/src/pages/Dashboard.tsx
+++ ui/src/pages/Dashboard.tsx
@@ -2,21 +2,30 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../store/hooks";
 import { createUser } from "../store/userSlice";
 import Contact from "../components/Contact";
-
+import Faq from "../components/Faq";
 import { Button } from '@mui/material';
 
-const buttons = [
+interface IButtonsInfo {
+  label: string;
+  navigation: string;
+  isDisabled: boolean;
+}
+
+const buttons: IButtonsInfo[] = [
   {
     label: "Play",
     navigation: "/singleplay",
+    isDisabled: false,
   },
   {
     label: "Create a room",
     navigation: "/createroom",
+    isDisabled: true,
   },
   {
     label: "Find a room",
     navigation: "/findroom",
+    isDisabled: true,
   },
 ];
 
@@ -35,13 +44,17 @@ const Dashboard = () => {
       <div className="main-buttons paper">
         {
           buttons.map((button, index) => (
-            <Button key={index} onClick={() => handleButtonInteraction(button.navigation, index)} color="secondary">
+            <Button 
+            disabled={button.isDisabled}
+            key={index}
+            onClick={() => handleButtonInteraction(button.navigation, index)} 
+            color="secondary">
               {button.label}
             </Button>
           ))
         }
       </div>
-
+      <Faq />
       <Contact />
 
     </>
