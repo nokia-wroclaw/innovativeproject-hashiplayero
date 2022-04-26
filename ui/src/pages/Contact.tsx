99bed59119edf4ebc787bcfc47db7236899d9bd3
Bartosz Szymczak <xbartosz2000@icloud.com>
2022-04-26 19:08:46 +0200

    Update Contact component

--- ui/src/pages/Contact.tsx
+++ ui/src/pages/Contact.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-const Contact = () => (
-    <div>
-        Form to Contact
-    </div>
-);
+const Contact = () => {
+
+    const emails = [
+        "email1", 
+        "email2", 
+        "email3" //Add team members' emails
+    ]
+
+    return (
+        <>
+            <h1>Contact</h1>
+            <h3>We're all ears.</h3>
+            <h5>In case you find any problems, or you would like to contacts us about anything, go ahead and do it.</h5>
+            <p>Our team members emails:</p>
+            {
+                emails.map((e) => (
+                    <ul>{e}</ul>
+                ))
+            }
+        </>
+    );
+};
 
 export default Contact;
