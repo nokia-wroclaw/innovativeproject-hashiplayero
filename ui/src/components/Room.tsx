0b292c2827aa74c524d51c478c68d1702fbca3bb
Pawel <pawir4@wp.pl>
2022-05-07 01:05:04 +0200

    Migrate WebSocket to Redux

--- ui/src/components/Room.tsx
+++ ui/src/components/Room.tsx
@@ -1,14 +1,52 @@
 import { IRoom } from "../interfaces/IRoom";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { RootState } from "../store/store";
+import { useAppDispatch } from "../store/hooks";
+import { useSelector } from "react-redux";
+import { setInitialRoomBoard } from "../store/RoomGameSlice";
 
 const Room = ({ room }: { room: IRoom }) => {
   const navigate = useNavigate();
+  const { webSocket } = useSelector((state: RootState) => state.webSocket);
+  const dispatch = useAppDispatch();
+  const { user } = useSelector((state: RootState) => state.defaultUser);
 
   const handleButtonInteraction = (navigation: String) => {
     // navigate(`${navigation}`);
     console.log(navigation);
   }
+
+  const handleChangeRoom = (roomName: string) => {
+    if (webSocket !== undefined) {
+      webSocket.send(
+        JSON.stringify({
+          action: "changeRoom",
+          userUuid: user.uuid,
+          data: {
+            roomName: roomName,
+          },
+        })
+      );
+      console.log("WebSocket-> Change Room");
+    }
+  };
+
+  const handleDisconnectRoom = () => {
+    if (webSocket !== undefined) {
+      webSocket.send(
+        JSON.stringify({
+          action: "changeRoom",
+          userUuid: user.uuid,
+          data: {
+            roomName: "lobby",
+          },
+        })
+      );
+      dispatch(setInitialRoomBoard());
+      console.log("WebSocket-> Change Room");
+    }
+  };
 
     return (
         <div className="paper">  
@@ -41,6 +79,7 @@ const Room = ({ room }: { room: IRoom }) => {
                 color="secondary"
                 onClick={() => {
                   handleButtonInteraction(`/room/${room.name}`);
+                  handleChangeRoom(room.name);
                 }}
               >
                   Join
---
+++ ui/src/store/WebSocketSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import IWebSocket from "../interfaces/IWebSocket";
+import { RootState } from "./store";
+
+const initialState: IWebSocket = {
+  webSocket: new WebSocket("ws://localhost:8080/ws/"),
+};
+
+export const WebSocketSlice = createSlice({
+  name: "webSocket",
+  initialState,
+  reducers: {
+    setWebSocket: (state, action: PayloadAction<IWebSocket>) => {
+        return action.payload;
+    },
+  },
+});
+
+export const { setWebSocket } = WebSocketSlice.actions;
+
+export const selectDefaultUser = (state: RootState) => state.defaultUser;
+
+export default WebSocketSlice.reducer;
