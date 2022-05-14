bc756aa7474e9b30252c867caa755e17c0056a36
Pawel <pawir4@wp.pl>
2022-05-14 13:18:14 +0200

    Fix and change structure of stateMachine

--- ui/src/interfaces/IStateMachine.tsx
+++ ui/src/interfaces/IStateMachine.tsx
@@ -1,6 +1,9 @@
 interface IStateMachine {
-    inRoom: boolean;
+    // inWaitingRoom: boolean;
     isAdmin: boolean;
+    inRoom: boolean; // out of date
+    // inSingleGame: boolean;
+    // inMultiGame: boolean;
 }
 
 export default IStateMachine;
