bf8d32862054c5e5d1c403648af528bb21a83c9f
Bartosz Szymczak <xbartosz2000@icloud.com>
2022-05-18 15:47:13 +0200

    Check if logo changes color

--- ui/src/__tests__/HeaderBar.test.jsx
+++ ui/src/__tests__/HeaderBar.test.jsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import {createMemoryHistory} from 'history'
 import {Router} from 'react-router-dom'
 import HeaderBar from '../components/HeaderBar';
@@ -24,4 +24,23 @@ test('HeaderBar renders with correct components', async () => {
     // Verify header content (Five active buttons)
     const items = await screen.findAllByRole('button')
     expect(items).toHaveLength(5)
+})
+
+test('HeaderBar logo changes color', async () => {
+    const history = createMemoryHistory()
+    const {getByRole} = render(
+        <Router location={history.location} navigator={history}>
+            <HeaderBar />
+        </Router>,
+    )
+
+    const logoFirstHalf = getByRole("heading", { name: /hashi/i })
+    expect(logoFirstHalf).toHaveStyle('color: 5452E4')
+    fireEvent.mouseOver(logoFirstHalf)
+    expect(logoFirstHalf).toHaveStyle('color: 7081A6')
+
+    const logoSecondHalf = getByRole("heading", { name: /playero/i })
+    expect(logoSecondHalf).toHaveStyle('color: 7081A6')
+    fireEvent.mouseOver(logoSecondHalf)
+    expect(logoSecondHalf).toHaveStyle('color: 5452E4')
 })
