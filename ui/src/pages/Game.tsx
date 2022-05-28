b55e09673fe8ff93ed553570afe77d1562c68aa1
rodakrafal <rafalrodak00@gmail.com>
2022-05-28 21:03:17 +0200

    Repair how the board is display
    Fix issue with unmounted component
    Add grid layout to board and additional information

--- ui/src/pages/Game.tsx
+++ ui/src/pages/Game.tsx
@@ -14,12 +14,18 @@ import {
 import Board from "./Board";
 import PlayerList from "../components/PlayerList";
 
+import { Grid, useMediaQuery } from '@mui/material';
+// import useMediaQuery from '@mui/material/useMediaQuery';
+
 const Game = () => {
   const { webSocket } = useSelector((state: RootState) => state.webSocket);
   const { user } = useSelector((state: RootState) => state.defaultUser);
   const dispatch = useAppDispatch();
   const { roomAndBoard } = useSelector((state: RootState) => state.RoomGame);
   const navigate = useNavigate();
+
+  const matches = useMediaQuery('(min-width:900px)');
+
   const { isBoardCorrect, inSingleGame, inWaitingRoom, isAdmin, inMultiGame } =
     useSelector((state: RootState) => state.StateMachine);
 
@@ -106,56 +112,65 @@ const Game = () => {
 
   return (
     <>
-      <div>
-        <Board />
-        <Button
-          onClick={() => {
-            handleCheckBoard();
-          }}
-        >
-          Check Board
-        </Button>
-        {!inSingleGame && inMultiGame ? (
-          <>
-            <Button
-              disabled={!isBoardCorrect}
-              onClick={() => {
-                handleFinishGame();
-              }}
-            >
-              Waiting Room
-            </Button>
-            <Button
-              onClick={() => {
-                handleExitGame();
-              }}
-            >
-              Exit Game
-            </Button>
-          </>
-        ) : null}
+      <Grid container columns={24} spacing={1}>
+        
+        <Grid item xs={3} sx={{ background: "blue", display: !matches ? "none" : "block" }}>
+          <span>xs</span>
+        </Grid>
+        <Grid item xs={24} md={18} sx={{ background: "pink" }}>
+          <Board />
+        </Grid>
+        <Grid item xs={3} sx={{ background: "blue", display: !matches ? "none" : "block" }}>
+          <span>xs</span>
+        </Grid>
+      </Grid>
+      <Button
+        onClick={() => {
+          handleCheckBoard();
+        }}
+      >
+        Check Board
+      </Button>
+      {!inSingleGame && inMultiGame ? (
+        <>
+          <Button
+            disabled={!isBoardCorrect}
+            onClick={() => {
+              handleFinishGame();
+            }}
+          >
+            Waiting Room
+          </Button>
+          <Button
+            onClick={() => {
+              handleExitGame();
+            }}
+          >
+            Exit Game
+          </Button>
+        </>
+      ) : null}
 
-        {inSingleGame && !inMultiGame ? (
-          <>
-            <Button
-              onClick={() => {
-                handleExitGameSingle();
-              }}
-            >
-              Exit Game
-            </Button>
-            <Button
-              disabled={!isBoardCorrect}
-              onClick={() => {
-                handlePlayAgain();
-              }}
-            >
-              Play Again
-            </Button>
-          </>
-        ) : null}
-        {!inSingleGame ? <PlayerList players={roomAndBoard.members} gameData={roomAndBoard.gameData} /> : null}
-      </div>
+      {inSingleGame && !inMultiGame ? (
+        <>
+          <Button
+            onClick={() => {
+              handleExitGameSingle();
+            }}
+          >
+            Exit Game
+          </Button>
+          <Button
+            disabled={!isBoardCorrect}
+            onClick={() => {
+              handlePlayAgain();
+            }}
+          >
+            Play Again
+          </Button>
+        </>
+      ) : null}
+      {!inSingleGame ? <PlayerList players={roomAndBoard.members} gameData={roomAndBoard.gameData} /> : null}
     </>
   );
 };
