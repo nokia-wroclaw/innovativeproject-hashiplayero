72cc920b47d8122cd211a1b5ac73e21bdb14fa3c
rodakrafal <rafalrodak00@gmail.com>
2022-05-29 19:53:46 +0200

    Add dialog box informing about correctly filled board

---
+++ ui/src/components/static-components/DialogWin.tsx
@@ -0,0 +1,119 @@
+import React, { useCallback, useRef, useState } from "react";
+import { DialogProps } from "@mui/material/Dialog";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+import { DialogTitle, DialogContentText, DialogContent, DialogActions, Dialog, Button } from "@mui/material";
+
+const canvasStyles = {
+    position: "fixed",
+    pointerEvents: "none",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0
+};
+
+
+const DialogWin = () => {
+    const [fullWidth] = useState(true);
+    const [maxWidth] = useState<DialogProps["maxWidth"]>("md");
+
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const refAnimationInstance = useRef<any>(null);
+
+    const getInstance = useCallback((instance) => {
+        refAnimationInstance.current = instance;
+    }, []);
+
+    const makeShot = useCallback((particleRatio, opts) => {
+        refAnimationInstance.current &&
+            refAnimationInstance.current({
+                ...opts,
+                origin: { y: 0.7 },
+                particleCount: Math.floor(200 * particleRatio)
+            });
+    }, []);
+
+    const fire = useCallback(() => {
+        makeShot(0.25, {
+            spread: 26,
+            startVelocity: 55
+        });
+
+        makeShot(0.2, {
+            spread: 60
+        });
+
+        makeShot(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 45
+        });
+    }, [makeShot]);
+
+    return (
+        <>
+            <Button
+                variant="outlined"
+                onClick={() => {
+                    handleClickOpen();
+                    fire();
+                }}
+            >
+                Open alert dialog
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending
+                        anonymous location data to Google, even when no apps are running.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <ReactCanvasConfetti refConfetti={getInstance} style={{
+                position: "fixed",
+                pointerEvents: "none",
+                width: "100%",
+                height: "100%",
+                top: 0,
+                left: 0
+            }} />
+        </>
+    );
+}
+export default DialogWin;
