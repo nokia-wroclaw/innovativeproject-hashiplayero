b97f535144ca600b830e89c19cd4c7d165fe8cea
Bartosz Szymczak <xbartosz2000@icloud.com>
2022-06-02 20:40:53 +0200

    Add basic database files
    Add puzzle to database everytime it's generated

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,18 @@
 package server
 
-import "os"
+import (
+	"database/sql"
+	"innovativeproject-hashiplayero/internal/server/puzzledb"
+	"log"
+	"os"
+
+	// To connect to an SQLite database using the mattn/go-sqlite3 driver, it is necessary to register it as the database/sql driver.
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const fileName = "sqlite.db"
+
+var puzzlesDB *puzzledb.SQLiteRepository
 
 func Start() {
 	router := setRouter()
@@ -8,6 +20,22 @@ func Start() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
+	}
+
+	// We want to run the program with a fresh instance each time, so this deletes the old database file if it exists.
+	os.Remove(fileName)
+
+	// Function registers the driver in the database/sql interface under the name sqlite3.
+	// Using the sql.Open() function with the registered sqlite3 driver name, we connect to a new SQLite database.
+	// The second argument is the data source name which in the case of SQLite is a path to the database file.
+	db, err := sql.Open("sqlite3", fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	puzzlesDB = puzzledb.NewSQLiteRepository(db)
+	if err := puzzlesDB.Migrate(); err != nil {
+		log.Fatal(err)
 	}
 
 	router.Run(":" + port)
